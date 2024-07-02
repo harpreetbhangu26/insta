7d83226a39953d7ca5b322febcc37dad39c76d86
harpreetbhangu26 <heronoone26@gmail.com>
2024-07-02 13:57:06 +0100

    foutrth

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -2,7 +2,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+const stripe = new Stripe(`${process.env.STRIPE_SECRET_KEY as string}`, {
   apiVersion: "2024-06-20",
 });
 
