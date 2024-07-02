af7424f714605fb4cc4be8e6dd6dbfe79e2b0545
harpreetbhangu26 <heronoone26@gmail.com>
2024-07-02 13:51:22 +0100

    thired

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     domains: ["static-ca-cdn.eporner.com"],
   },
+  env: {
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+  },
 };
 
 export default nextConfig;
