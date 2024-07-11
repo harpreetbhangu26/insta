ead2b7f8aa259e591dc66315c6d3bd9f04f165b3
harpreetbhangu26 <heronoone26@gmail.com>
2024-07-11 15:11:34 +0100

    added youtube phote

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,12 +19,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} h-screen `}>
-        <div className="flex h-full border-r-green-500">
-          <Sidebar />
-          <div className="w-2 bg-green-500 hidden md:block lg:block"></div>
-          <div className="flex flex-col flex-1">
+        <div className="flex h-full ">
+          <div className="flex flex-col flex-1 ">
             <div className="flex-1 overflow-auto   ">{children}</div>
-            <BottomBar />
+            <div className="mb-5">
+              <BottomBar />
+            </div>
           </div>
         </div>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,24 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="p-4 text-black">
-      <div className="border-green-600 bg-green-500 p-4 text-2xl flex items-center justify-between  ">
-        <h1 className="bg-white p-2 rounded-md">Learn Punjabi lanuguage</h1>
-        <div className="skew-y-6 ">
-          <Image src="/punjab.png" alt="punjab " height={70} width={90} />
-        </div>
+    <div className="p-4 ">
+      <div className=" flex flex-col items-center justify-center">
+        <h1 className="text-2xl text-white bg-red-500 p-3  rounded-md">
+          Watch Youtube Videos without advertisements
+        </h1>
+        <Image
+          className=""
+          src="/youtube.svg"
+          alt="youtube"
+          height={400}
+          width={500}
+        />
       </div>
-      <div className="mt-6">
-        <Parts />
+      <div className="mt-52 ml-28">
+        <h1 className="text-xl bg-gray-800 text-white p-2 rounded-md w-64">
+          Click on the search Button
+        </h1>
+        <Image src="/downarrow.svg" alt="arrow" height={200} width={100} />
       </div>
     </div>
   );
